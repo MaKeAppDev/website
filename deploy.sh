4749a84aafb99b7441f84d9553d7635985cf79cf
Markus Hofbauer <markus.hofbauer@tum.de>
2020-10-21 15:37:26 +0200

    Create ci.yml (#3)
    * Create ci.yml
    
    * configure lint
    
    * npm audit fix
    
    * disable js linters for superlint
    
    * add symlink for editorconfig
    
    * fix shellcheck

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+set -e
+
 HOST=$1
 CLEAR_SCRIPT="clear_build_products.sh"
 
-if [[ $# -eq 0 ]] ; then
-    echo 'ERROR: No host for deployment provided'
-    exit 1
+if [[ $# -eq 0 ]]; then
+	echo 'ERROR: No host for deployment provided'
+	exit 1
 fi
 
 cp $CLEAR_SCRIPT dist/
@@ -13,7 +15,8 @@ chmod +x dist/$CLEAR_SCRIPT
 
 echo "Clearing old version on $HOST"
 scp dist/$CLEAR_SCRIPT "$HOST":~/html/
-ssh $HOST "cd ~/html/ && ./$CLEAR_SCRIPT"
+# shellcheck disable=SC2029
+ssh "$HOST" "cd ~/html/ && ./$CLEAR_SCRIPT"
 
 echo "Deploy to $HOST"
 scp -r dist/* "$HOST":~/html/
