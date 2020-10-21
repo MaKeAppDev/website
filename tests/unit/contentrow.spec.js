68b644c41a89e80e745424c006cf25362fcc9088
Markus Hofbauer <markus.hofbauer@tum.de>
2020-10-21 17:50:34 +0200

    remove test

--- tests/unit/contentrow.spec.js
+++ tests/unit/contentrow.spec.js
@@ -3,12 +3,6 @@ import { shallowMount } from '@vue/test-utils';
 import ContentRow from '@/components/ContentRow.vue';
 
 describe('ContentRow.vue', () => {
-  it('Passing title property correctly', () => {
-    const wrapper = shallowMount(ContentRow);
-    wrapper.setProps({ title: 'Test Title' });
-    expect(wrapper.text()).to.include('Test Title');
-    expect(wrapper.contains('lead'));
-  });
   it('Passing slot content correctly', () => {
     const wrapper = shallowMount(ContentRow, {
       slots: {
@@ -16,6 +10,6 @@ describe('ContentRow.vue', () => {
       },
     });
     expect(wrapper.text()).to.include('Div Content');
-    expect(wrapper.contains('.fancy-div'));
+    expect(wrapper.find('.fancy-div'));
   });
 });
