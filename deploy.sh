31ab64b242ce5f5cf534db63edc69b2d449ec454
Kevin Meyer <kevinmeyer@me.com>
2020-10-21 15:16:34 +0200

    deploy: run clear script prior to copy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 HOST=$1
+CLEAR_SCRIPT="clear_build_products.sh"
 
 if [[ $# -eq 0 ]] ; then
     echo 'ERROR: No host for deployment provided'
     exit 1
 fi
 
-cp clear_build_products.sh dist/
-chmod +x dist/clear_build_products.sh
+cp $CLEAR_SCRIPT dist/
+chmod +x dist/$CLEAR_SCRIPT
+
+echo "Clearing old version on $HOST"
+scp dist/$CLEAR_SCRIPT "$HOST":~/html/
+ssh $HOST "cd ~/html/ && ./$CLEAR_SCRIPT"
 
 echo "Deploy to $HOST"
 scp -r dist/* "$HOST":~/html/
